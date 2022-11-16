7caacc3b620e60283b0b602d6307a90ddca43fa8
iamisha <isha.201710@ncit.edu.np>
2022-11-16 23:09:06 +0545

    ArithmeticException code written

---
+++ java -1st/Division.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+class Division {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the first integer value:-");
+        int a = sc.nextInt();
+        System.out.println("Enter the second integer value:-");
+        int b = sc.nextInt();
+        try {
+            int ans = a / b;
+            System.out.println("Your answer is " + ans);
+        } catch (ArithmeticException e) {
+            System.out.println("Please enter the valid integers.");
+        }
+
+    }
+}
