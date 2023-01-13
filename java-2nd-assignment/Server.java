28810834c754a355985fe6a6a1bf05a136cfc3aa
iamisha <isha.201710@ncit.edu.np>
2023-01-13 10:07:35 +0545

    assignment second folder added :mag_right:

---
+++ java-2nd-assignment/Server.java
@@ -0,0 +1,40 @@
+//importing all the packages
+import java.net.*;
+import java.io.*;
+
+
+class Server{
+    public static void main(String[] args) {
+        try{
+            
+            ServerSocket ss = new ServerSocket(5667);
+            Socket socket = ss.accept();
+
+            PrintWriter pw = new PrintWriter(socket.getOutputStream(), true);
+            BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            String  msg;
+            while((msg = br.readLine()) != null){
+                if(isPalindrome(msg)){
+                    pw.println("palindrome");
+                }else{
+                    pw.println("not a palindrome");
+                }
+            }
+        
+        }catch(IOException e){
+            System.out.println(e.getMessage());
+        }
+    }
+    public static  boolean isPalindrome(String str){
+        int  i = 0, j = str.length() - 1;
+        while(i < j){
+            if(str.charAt(i) != str.charAt(j)){
+                return false;
+            }
+            i ++;
+            j--;
+        }
+    
+    return false;
+}
+}
